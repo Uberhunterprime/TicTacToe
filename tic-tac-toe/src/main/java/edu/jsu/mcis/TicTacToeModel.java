3df377acf72838f9b830f7deada1dda50347117f
Uberhunterprime <c9QeUt4bnMbDExBDetJckLW>
2017-01-30 17:44:32 -0600

    update

--- tic-tac-toe/src/main/java/edu/jsu/mcis/TicTacToeModel.java
+++ tic-tac-toe/src/main/java/edu/jsu/mcis/TicTacToeModel.java
@@ -1,6 +1,121 @@
 package edu.jsu.mcis;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.net.URL;
+import javax.swing.AbstractButton;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class TicTacToeModel {
+public class TicTacToeModel extends JPanel{
+    
+//    protected static JButton topLbn, topMbn, topRbn, ctrLbn, ctrMbn, ctrRbn, btmLbn, btmMbn, btmRbn;
+//    public JButtonGrid(){
+        
+        JButton[][] buttonGrid = new JButton[3][3];
+        
+//        topLbn = new JButton("_");
+//        topLbn.setVerticalTextPosition(AbstractButton.TOP);
+//        topLbn.setHorizontalTextPosition(AbstractButton.LEADING);
+//        topLbn.addActionListiener (new ActionListener(){
+            
+//            public void actionperformed(ActionEvent e){
+            
+//            }
+//        });
+        
+//        topMbn = new JButton("_");
+//        topMbn.setVerticalTextPosition(AbstractButton.TOP);
+//        topMbn.setHorizontalTextPosition(AbstractButton.CENTER);
+//        topMbn.addActionListiener (new ActionListener(){
+            
+//            public void actionperformed(ActionEvent e){
+            
+//            }
+//        });
+        
+        
+//        topRbn = new JButton("_");
+//        topRbn.setVerticalTextPosition(AbstractButton.TOP);
+//        topRbn.setHorizontalTextPosition(AbstractButton.TRAILING);
+//        topRbn.addActionListiener (new ActionListener(){
+            
+//            public void actionperformed(ActionEvent e){
+            
+//            }
+//        });
+        
+        
+//        ctrLbn = new JButton("_");
+//        ctrLbn.setVerticalTextPosition(AbstractButton.CENTER);
+//        ctrLbn.setHorizontalTextPosition(AbstractButton.LEADING);
+//        ctrLbn.addActionListiener (new ActionListener(){
+            
+//            public void actionperformed(ActionEvent e){
+            
+//            }
+//        });
+        
+        
+//        ctrMbn = new JButton("_");
+//        ctrMbn.setVerticalTextPosition(AbstractButton.CENTER);
+//        ctrMbn.setHorizontalTextPosition(AbstractButton.CENTER);
+//        ctrMbn.addActionListiener (new ActionListener(){
+            
+//            public void actionperformed(ActionEvent e){
+            
+//            }
+//        });
+        
+        
+//        ctrRbn = new JButton("_");
+//        ctrRbn.setVerticalTextPosition(AbstractButton.CENTER);
+//        ctrRbn.setHorizontalTextPosition(AbstractButton.TRAILING);
+//        ctrRbn.addActionListiener (new ActionListener(){
+            
+//            public void actionperformed(ActionEvent e){
+            
+//            }
+//        });
+        
+        
+//        btmLbn = new JButton("_");
+//        btmLbn.setVerticalTextPosition(AbstractButton.BOTTOM);
+//        btmLbn.setHorizontalTextPosition(AbstractButton.LEADING);
+//        btmLbn.addActionListiener (new ActionListener(){
+            
+//            public void actionperformed(ActionEvent e){
+            
+//            }
+//        });
+        
+        
+//        btmMbn = new JButton("_");
+//        btmMbn.setVerticalTextPosition(AbstractButton.BOTTOM);
+//        btmMbn.setHorizontalTextPosition(AbstractButton.CENTER);
+//        btmMbn.addActionListiener (new ActionListener(){
+            
+//            public void actionperformed(ActionEvent e){
+            
+//            }
+//        });
+        
+        
+//        btmRbn = new JButton("_");
+//        btmRbn.setVerticalTextPosition(AbstractButton.BOTTOM);
+//        btmRbn.setHorizontalTextPosition(AbstractButton.TRAILING);
+//        btmRbn.addActionListiener (new ActionListener(){
+            
+//            public void actionperformed(ActionEvent e){
+            
+//            }
+//        });
+        
+    
+    
+
     public enum Mark { EMPTY, X, O };
     private Mark[][] board;
     private boolean xTurn;
@@ -101,5 +216,13 @@ public class TicTacToeModel {
             }
         }
                 
-		
+
+    	public static void actionPerformed(ActionEvent e){
+            JButton.setText(m);
+            JButton.setEnable(false);
+            board.setMark(buttonGrid);
+            getWinner();
+            boardFull();
+            GameOver();
+	}
 }
